0aff54f5e8338711fa72fb91ef8a1c7821990c2a
Todd Eagle <todd.eagle@gmail.com>
2020-08-17 10:10:02 -0400

    Component styling

--- src/styles/Layout/StyledProfileHeader.js
+++ src/styles/Layout/StyledProfileHeader.js
@@ -6,7 +6,7 @@ export const StyledProfileHeader = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    height: 18rem;
+    height: 24rem;
     background-color: #fff;
     border-bottom: #a3a3a3 solid 2px;
     background-image:  linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url(${profileBackground});
@@ -15,12 +15,12 @@ export const StyledProfileHeader = styled.div`
 `
 export const ProfileHeaderImage = styled.img`
     border-radius: 50%;
-    height: 10rem;
-    width: 10rem;
+    height: 12rem;
+    width: 12rem;
     box-shadow: 0px 7px 13px -1px rgba(0,0,0,0.75);
 `
 export const ProfileHeaderText = styled.p`
-    font-size: 1.5rem;
+    font-size: 1.8rem;
     font-weight: 600;
     margin-top: 1rem;
 `
