9a4ef5fae8939fbaf70605ef6fafdea10875eeea
Todd Eagle <todd.eagle@gmail.com>
2020-09-16 15:49:07 -0400

    Adding media queries

--- src/styles/Pages/Profile.js
+++ src/styles/Pages/Profile.js
@@ -4,9 +4,7 @@ export const PageWrapper = styled.div`
     display: flex;
     justify-content: center;
     padding-top: 10rem;
-    height: 75vh;
     flex-wrap: wrap;
-
 `
 export const ProfileImage = styled.img`
     width: 30rem;
@@ -14,15 +12,34 @@ export const ProfileImage = styled.img`
     border-radius: 50%;
     cursor: pointer;
 
+    @media only screen and (max-width: 100rem) {
+        width: 25rem;
+        height: 25rem;
+    }
+
+    @media only screen and (max-width: 80rem) {
+        width: 20rem;
+        height: 20rem;
+    }
+
     ${(props) =>props.imageType ==='background' && css`
         width:auto;
         border-radius: 5px;
+
+        @media only screen and (max-width: 100rem) {
+            width:auto;
+        }
     `
     }
 `
 export const ProfileContainer = styled.div`
     display: flex;
+    margin-top: 4rem;
     max-width: 130rem;
+    @media only screen and (max-width: 50rem) {
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 `
 export const ProfileBackgroundImage = styled.img`
     height: 25rem;
@@ -41,6 +58,7 @@ export const UploadWrapper = styled.div`
 `
 export const ProfileText = styled.div`
     text-align: center;
+    padding: 0 2rem;
     font-size: 2rem;
     height: 2.5rem;
     flex-basis: 100%;
