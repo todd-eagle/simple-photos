e0f1332d299df8a94bb65af1bc273f7053b79efe
Todd Eagle <todd.eagle@gmail.com>
2020-10-07 22:01:43 -0400

    Changes for hosting

--- src/components/ImagesContainer/ImagesContainer.js
+++ src/components/ImagesContainer/ImagesContainer.js
@@ -2,22 +2,33 @@ import React from 'react'
 import {LandingImageContainter,
     LandingImageCard, ImageWrapper} from '../../styles/Pages/StyledLandingPage'
 import {CardImages} from '../../styles/Components/Cards'
-import { withRouter } from 'react-router-dom'
+import { Redirect, withRouter } from 'react-router-dom'
+
 const ImagesContainer = (props) => {
 //  console.log(" props.location: ", props.location)
     const {imageData, handleSelectImageFn} = props
-    let images = imageData.map( el =>{
-        return <LandingImageCard key={el.title} onClick={()=>handleSelectImageFn({el})}>
-                        <CardImages loading="lazy" src={el.link} alt={el.title}/>
-                </LandingImageCard>           
-    })
+
+    let images=null
+
+    if(imageData!==undefined){
+        images = imageData.map( el =>{
+            return <LandingImageCard key={el.title} onClick={()=>handleSelectImageFn({el})}>
+                            <CardImages loading="lazy" src={el.link} alt={el.title}/>
+                    </LandingImageCard>           
+        })
+    }
 
     return(
-        <ImageWrapper>
-            <LandingImageContainter>
-                {images}
-            </LandingImageContainter>
-        </ImageWrapper>
+        <>
+        {images ?
+            <ImageWrapper>
+                <LandingImageContainter>
+                    {images}
+                </LandingImageContainter>
+            </ImageWrapper>
+        : 
+        <Redirect to='/' />}
+        </>
         )
 }
 
