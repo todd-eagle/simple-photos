297026c2165e5e263404424f54c7c88845e03afb
Todd Eagle <todd.eagle@gmail.com>
2020-10-12 02:24:24 -0400

    Fixed error in server/index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(fileUpload());
 app.use( express.static( `${__dirname}/../build` ) )
 
 // production server
-app.use(express.static(__dirname + '/../build'))
+// app.use(express.static(__dirname + '/../build'))
 
 // app.get('*', (req, res)=>{
 //     res.sendFile(path.join(__dirname, '../build/index.html'))
