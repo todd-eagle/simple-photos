d4229cba3cb201ffc53de15977691070bec1de64
Todd Eagle <todd.eagle@gmail.com>
2020-10-14 16:48:40 -0400

    Adding SendGrid Functionality

--- server/controllers/mail-controller.js
+++ server/controllers/mail-controller.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail')
-const templateID = 'd-3519d20db21847249db89e02de170122' 
+const templateID = 'd-3519d20db21847249db89e02de170122'
 const fromEmail = 'no-reply@thecrackle.us'
 
 module.exports = {
     sendMail: (req, res) => {
         const {email} = req.body
-        
+        console.log("email: ", email)
         sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
         const msg = {
@@ -24,12 +24,5 @@ module.exports = {
           console.error(error)
         })
 
-        // const msg = {
-        //   to: `${email}`,
-        //   from: `${fromEmail}`,
-        //   templateId: templateID
-        // }
-        // const sentMail = await sgMail.send(msg)
-        // sentMail ? res.status(200).send('Email sent') : res.status(500).send('Server error: mail not sent')
     }
 }
