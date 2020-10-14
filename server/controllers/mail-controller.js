4f9799555ab452a165b22a84febdb1af87697117
Todd Eagle <todd.eagle@gmail.com>
2020-10-14 13:26:50 -0400

    Adding SendGrid Functionality

--- server/controllers/mail-controller.js
+++ server/controllers/mail-controller.js
@@ -4,22 +4,32 @@ const templateID = 'd-3519d20db21847249db89e02de170122'
 const fromEmail = 'no-reply@thecrackle.us'
 
 module.exports = {
-    sendMail: (req, res) => {
+    sendMail: async(req, res) => {
         const {email} = req.body
 
         const msg = {
-          to: `${email}`,
-          from: `${fromEmail}`,
-          templateId: templateID
+          to: 'test@example.com', // Change to your recipient
+          from: 'test@example.com', // Change to your verified sender
+          subject: 'Sending with SendGrid is Fun',
+          text: 'and easy to do anywhere, even with Node.js',
+          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
         }
-        // const sentMail = await sgMail.send(msg)
-        // sentMail ? res.status(200).send('Email sent') : res.status(500).send('Server error: mail not sent')
-        sgMail.send(msg)
+
+        sgMail
+        .send(msg)
         .then(() => {
           console.log('Email sent')
         })
         .catch((error) => {
           console.error(error)
         })
+
+        // const msg = {
+        //   to: `${email}`,
+        //   from: `${fromEmail}`,
+        //   templateId: templateID
+        // }
+        // const sentMail = await sgMail.send(msg)
+        // sentMail ? res.status(200).send('Email sent') : res.status(500).send('Server error: mail not sent')
     }
 }
