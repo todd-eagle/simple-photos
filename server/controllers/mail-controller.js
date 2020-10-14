d7f103ef1bca8c468a743636dc8f969567ae02bf
Todd Eagle <todd.eagle@gmail.com>
2020-10-14 02:27:25 -0400

    Adding SendGrid Functionality

--- server/controllers/mail-controller.js
+++ server/controllers/mail-controller.js
@@ -1,44 +1,20 @@
 const transporter = require('../index')
 const nodemailer = require("nodemailer")
-const hbs = require('nodemailer-express-handlebars')
-
-const {SERVER_PORT, EMAIL_PORT, SMTP_SERVER, USERNAME, PASSWORD} = process.env;
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const templateID = 'd-3519d20db21847249db89e02de170122' 
+const fromEmail = 'no-reply@thecrackle.us'
 
 module.exports = {
     sendMail: async(req, res) => {
         const {email} = req.body
 
-        const transporter = nodemailer.createTransport({
-            service: "SendinBlue",
-            auth: {
-              user: `${USERNAME}`,
-              pass: `${PASSWORD}` 
-            }
-          })
-
-          transporter.use('compile', hbs({
-            viewEngine:'express-handlebars',
-            viewPath: '../views/'
-          }))
-          
-          transporter.verify(function(error, success) {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log("Server is ready to take our messages!");
-              }
-            })
-
-        if(email){
-            let info = await transporter.sendMail({
-                from: '"Todd Eagle" <todd.eagle@gmail.com>',
-                to: `${email}, ${email}`, 
-                subject: "Welcome to SimplePhotos!", 
-                template: 'index'
-              })
-            info ? res.status(200).send('email sent') : res.status(500).send('email not sent')
-        } else {res.status(404).send('email not found')}
-        // email ? res.status(200).send('email sent'):
-        //res.status(500).send('email not sent')
+        const msg = {
+          to: `${email}`,
+          from: `${fromEmail}`,
+          templateId: templateID
+        }
+        const sentMail = await sgMail.send(msg)
+        sentMail ? res.status(200).send('Email sent') : res.status(500).send('Server error: mail not sent')
     }
 }
