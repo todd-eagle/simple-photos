e1e5fa05a7e6393e14c6456c23bc20eb8bf57633
Todd Eagle <todd.eagle@gmail.com>
2020-10-14 16:34:07 -0400

    Adding SendGrid Functionality

--- server/controllers/mail-controller.js
+++ server/controllers/mail-controller.js
@@ -6,15 +6,13 @@ const fromEmail = 'no-reply@thecrackle.us'
 module.exports = {
     sendMail: async(req, res) => {
         const {email} = req.body
-
+        
         sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
         const msg = {
           to: `${email}`, // Change to your recipient
           from: `${fromEmail}`, // Change to your verified sender
-          subject: 'Sending with SendGrid is Fun',
-          text: 'and easy to do anywhere, even with Node.js',
-          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+          templateId: templateID
         }
 
         sgMail
--- server/sendGridTest.js
+++ server/sendGridTest.js
@@ -1,22 +1,19 @@
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail')
-const templateID = 'd-3519d20db21847249db89e02de170122' 
+const templateID = 'd-3519d20db21847249db89e02de170122'
 const fromEmail = 'no-reply@thecrackle.us'
 
-const {email} = 'todd.eagle@outlook.com'
+const email = 'todd.eagle@outlook.com'
 
 const {SENDGRID_API_KEY} = process.env
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-console.log()
 
 const msg = {
   to: `${email}`, // Change to your recipient
   from: `${fromEmail}`, // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
+  templateId: templateID
+  }
 
 sgMail
 .send(msg)
